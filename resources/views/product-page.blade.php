998502d18421732c79014d7ee54d2e143badc7e5
skashama <shaloombowa@gmail.com>
2020-07-12 23:15:08 -0700

    include modals in solution and product page

--- resources/views/product-page.blade.php
+++ resources/views/product-page.blade.php
@@ -14,6 +14,71 @@ $group4 = get_field('featured_image_group4');
 $headerImage = get_field('page_header');
 
 // if( $group1 ): ?>
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Lead Carbon Battery Core Technology</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Description</th>
+                    <th scope="col">Operation Mode</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Negative Electrode Carbon Technology</td>
+                    <td>Adding carbon materials, which is with
+                        Superelevation surface area and
+                        Superconductivity, into negative electrode
+                        formula can restrain the sulfation of
+                        negative electrode active materials,
+                        improve electrode’s low temperature
+                        performance and charging
+                        acceptance. performance.</td>
+                  </tr>
+                  <tr>
+                    <td>Special Positive Electrode Active Substance Structure</td>
+                    <td>The network structure of positive electrode
+                        active materials can effectively inhibit the
+                        softening of positive electrode lead paste,
+                        improve high current charge-discharge
+                        performance and charging acceptance
+                        capacity.</td>
+                  </tr>
+                  <tr>
+                    <td>Corrosion-resistant Alloy Technology</td>
+                    <td>With strong corrosion resistance, creep
+                        resistance and electrochemical properties,
+                        a complete fluid collector structure is
+                        maintained during the battery usage cycle,
+                        increasing the hydrogen and oxygen
+                        evolution potential, reducing water loss
+                        and increasing life span.</td>
+                  </tr>
+                  <tr>
+                    <td>Battery Separator with Hydrophobic Group</td>
+                    <td>With the features of Patent third electrode,
+                        ultra-fine glass fiber AGM + hydrophobic
+                        polymer polyolefin fiber, ultrastrong
+                        stretch-proof properties, stable oxygen
+                        recombination channel, and durable pressure
+                        maintenance performane, the battery service
+                        life is improved.</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <div class="page-header" style="width: 100%;">
     <img src="<?php echo esc_url( $headerImage['url'] ); ?>" class="img-fluid" alt="<?php echo esc_attr( $headerImage['alt'] ); ?>" />
@@ -39,6 +104,9 @@ $headerImage = get_field('page_header');
                 <img src="<?php echo esc_url( $group2['featured_image']['url'] ); ?>" alt="<?php echo esc_attr( $hero['featured_image']['alt'] ); ?>" />
                 <h3><?= $group2['featured_image_title'] ?></h3>
                 <p><?= $group2['feature_image_description'] ?></p>
+                <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#staticBackdrop">
+                    Launch modal
+                </button>                  
             </div>
 
         </div>
--- resources/views/solution-page.blade.php
+++ resources/views/solution-page.blade.php
@@ -14,7 +14,127 @@
     $group5 = get_field('featured_solution_group_5');
     $group6 = get_field('featured_solution_group_6');
     $headerImage = get_field('page_header');
+    $groupOneModal = get_field('featured_solution_modal_group_1');
+    $groupfiveModal = get_field('featured_solution_modal_group_5');
+    $groupsixModal = get_field('featured_solution_modal_group_6');
 ?>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="groupOneModal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel"><?= $groupOneModal['featured_image_title'] ?></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img style="display: block; margin: auto;" src="<?php echo esc_url( $groupOneModal['featured_image']['url'] ); ?>" class="img-fluid" alt="<?php echo esc_attr( $hero['featured_image']['alt'] ); ?>" />
+        </div>
+        <div class="modal-footer">
+            <?= $groupOneModal['featured_image_description'] ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="groupTwoModal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">EMS Management System</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Functions</th>
+                    <th scope="col">Monitoring Standards</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Remote monitoring</td>
+                    <td>Once/24h (adjustable)</td>
+                  </tr>
+                  <tr>
+                    <td>Remote management</td>
+                    <td>Yes</td>
+                  </tr>
+                  <tr>
+                    <td>Data backup</td>
+                    <td>Internal resistance data is kept for 1 year and other data for half a year</td>
+                  </tr>
+                  <tr>
+                    <td>Alarm information</td>
+                    <td>60 cycles</td>
+                  </tr>
+                  <tr>
+                    <td>Analysis report</td>
+                    <td>Once/month</td>
+                  </tr>
+                  <tr>
+                    <td>Information push</td>
+                    <td>Yes</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="groupFiveModal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel"><?= $groupfiveModal['featured_title'] ?></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <p><?= $groupfiveModal['featured_text'] ?></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="groupSixModal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">ESS Topology</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img src="<?php echo esc_url( $groupsixModal['featured_image_one']['url'] ); ?>" class="img-fluid" alt="<?php echo esc_attr( $hero['featured_image']['alt'] ); ?>" />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="groupSix_Modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Container Layout</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img src="<?php echo esc_url( $groupsixModal['featured_image_two']['url'] ); ?>" class="img-fluid" alt="<?php echo esc_attr( $hero['featured_image']['alt'] ); ?>" />
+        </div>
+      </div>
+    </div>
+  </div>
 
 <div class="page-header" style="width: 100%;">
     <img src="<?php echo esc_url( $headerImage['url'] ); ?>" class="img-fluid" alt="<?php echo esc_attr( $headerImage['alt'] ); ?>" />
@@ -35,6 +155,9 @@
             <div class="card-body">
                 <h5 class="card-title"><?= $group1['featured_image_title'] ?></h5>
                 <p class="card-text"><?= $group1['featured_image_description'] ?></p>
+                <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#groupOneModal">
+                    Launch modal
+                </button>
             </div>
             </div>
         </div>
@@ -44,6 +167,9 @@
             <div class="card-body">
                 <h5 class="card-title"><?= $group2['featured_image_title'] ?></h5>
                 <p class="card-text"><?= $group2['featured_image_description'] ?></p>
+                <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#groupTwoModal">
+                    Description
+                </button>
             </div>
             </div>
         </div>
@@ -74,6 +200,9 @@
             <div class="card-body">
                 <h5 class="card-title"><?= $group5['featured_image_title'] ?></h5>
                 <p class="card-text"><?= $group5['featured_image_description'] ?></p>
+                <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#groupFiveModal">
+                    Launch modal
+                </button>
             </div>
             </div>
         </div>
@@ -83,6 +212,12 @@
             <div class="card-body">
                 <h5 class="card-title"><?= $group6['featured_image_title'] ?></h5>
                 <p class="card-text"><?= $group6['featured_image_description'] ?></p>
+                <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#groupSixModal">
+                    Launch modal One
+                </button>
+                <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#groupSix_Modal">
+                    Launch modal Two
+                </button>
             </div>
             </div>
         </div>
